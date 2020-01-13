8e162e435afb98795884bc33e393c3f7b12ae1a8
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-13 02:39:54 +0000

    recursion

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
-}
 
-module.exports = sumItems;
+  return array.reduce((element, i) => {
+    if (Array.isArray(i)) {
+      return element + sumItems(i);
+    }
+    return element + i;
+  }, 0)
+};
+
+sumItems([1, 2, 3, 4, 5]); // console.log() => 15
+sumItems([[1, 2, [[3], 4]], 5, []]); // console.log() => 15
+sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]);// console.log()=> 1 
+
+
